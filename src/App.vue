4cee20687394d5eb3f03c28f199b685b3c88832f
Thomas <thomasloye1@gmail.com>
2025-05-23 11:55:07 +0200

    feat : bandeau page d'accueil

--- src/App.vue
+++ src/App.vue
@@ -79,3 +79,18 @@ onMounted(async () => {
 
   <Footer/>
 </template>
+
+<style>
+body::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url('@/assets/img/backgrounds/squares.jpg') repeat;
+  background-size: 1000px 500px;
+  opacity: 0.2;
+  z-index: -1;
+}
+</style>
--- src/views/VueAccueil.vue
+++ src/views/VueAccueil.vue
@@ -1,14 +1,38 @@
-﻿
-<script setup lang="ts">
+﻿<script setup lang="ts">
 
 </script>
 
 <template>
-  <h1>Ceci est la vue accueil de la v1.9.4</h1>
-  <p>La barre de téléchargement moderne est maintenant opérationnelle !</p>
-  <p>Test modernité</p>
+  <section class="main-pres">
+    <h1>FluoGraphiX : l'outil de visualisation et de traitement de données issues de fluorimètres</h1>
+    <span class="bouton-main">DÉCOUVRIR</span>
+
+    <div class="wrap-cards">
+      <div class="card">
+        <span class="color"></span>
+        <span>Visualisez, corrigez et convertissez vos données de mesure</span>
+        <a class="animated-link" href="">Voir plus <img src="@/assets/img/icons/fleche-droite.png" alt=""></a>
+      </div>
+
+      <div class="card">
+        <span class="color"></span>
+        <span>Consultez vos données de calibration et vérifiez leur conformité</span>
+        <a class="animated-link" href="">Voir plus <img src="@/assets/img/icons/fleche-droite.png" alt=""></a>
+      </div>
+
+      <div class="card">
+        <span class="color"></span>
+        <span>Consultez la documentation pour obtenir les réponses à vos questions</span>
+        <a class="animated-link" href="">Voir plus <img src="@/assets/img/icons/fleche-droite.png" alt=""></a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+@import "@/assets/styles/styleAccueil.css";
 
+.main-pres {
+  background-image: url('@/assets/img/hydro_pictures/img6.jpg');
+}
 </style>
