df3108abf10e966a1542523a904b29ef6f8fe5fe
Thomas <thomasloye1@gmail.com>
2025-05-26 14:10:19 +0200

    feat : premiere version de la vueTelecharger

--- src/views/VueCalibration.vue
+++ src/views/VueCalibration.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-
+  <img src="@/assets/img/icons/windows.png" style="height: 50px" alt="">
 </template>
 
 <style scoped>
--- src/views/VueTelecharger.vue
+++ src/views/VueTelecharger.vue
@@ -1,12 +1,107 @@
-﻿
-<script setup lang="ts">
+﻿<script setup lang="ts">
 
 </script>
 
 <template>
+  <section class="glassmorphism">
 
+    <div class="glasmorphism-wrap">
+      <div class="left">
+        <img src="@/assets/img/cosmetics/brgm.svg" class="logo" alt="">
+        <img src="@/assets/img/illus/download.png" alt="">
+      </div>
+
+
+      <div class="right">
+        <div class="presentation">
+          <h2>Télécharger <span class="orange">FluoGraphiX</span></h2>
+          <span class="texte-gris-simple">Téléchargez et installez maintenant FluoGraphiX sur votre appareil. Bienvenue sur FluoGraphiX !</span>
+        </div>
+
+        <div class="possibility">
+          <img src="@/assets/img/icons/windows.png" alt="">
+          <div>
+            <span class="color"></span>
+            <h4>FluoGraphiX pour <span class="orange">Windows</span></h4>
+            <span class="bouton boutonFonce">TÉLÉCHARGER</span>
+          </div>
+        </div>
+        <div class="possibility">
+          <img src="@/assets/img/icons/mac.png" alt="">
+          <div>
+            <span class="color"></span>
+            <h4>FluoGraphiX pour <span class="orange">Mac</span></h4>
+            <span class="bouton boutonFonce">TÉLÉCHARGER</span>
+          </div>
+        </div>
+        <div class="possibility">
+          <img src="@/assets/img/icons/linux.png" alt="">
+          <div>
+            <span class="color"></span>
+            <h4>FluoGraphiX pour <span class="orange">Linux</span></h4>
+            <span class="bouton boutonFonce">TÉLÉCHARGER</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+@import "@/assets/styles/glassmorphism.css";
 
+.glassmorphism {
+  background-image: url('@/assets/img/hydro_pictures/img10.jpg');
+}
+
+.possibility {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  border: 2px solid #737373;
+  border-radius: 15px;
+  width: 400px;
+  padding: 10px;
+
+  img {
+    height: 50px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    .color {
+      width: 70px;
+      height: 8px;
+      border-radius: 20px;
+    }
+
+    .boutonFonce {
+      padding: 0 10px !important;
+      font-size: 0.8em;
+      font-weight: 300;
+      width: fit-content;
+    }
+  }
+
+  div:nth-child(1) {
+    .color {
+      background-color: var(--vertBRGM);
+    }
+  }
+
+  div:nth-child(2) {
+    .color {
+      background-color: var(--roseBRGM);
+    }
+  }
+
+  div:nth-child(3) {
+    .color {
+      background-color: var(--bleuBRGM);
+    }
+  }
+}
 </style>
