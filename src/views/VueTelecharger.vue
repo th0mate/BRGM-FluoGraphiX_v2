4f311e26a23ab3f4e402958b89119f842e48d963
Thomas <thomasloye1@gmail.com>
2025-07-02 16:13:21 +0200

    feat : téléchargement latest depuis version hébergée

--- src/views/VueTelecharger.vue
+++ src/views/VueTelecharger.vue
@@ -1,7 +1,35 @@
 ﻿<script setup lang="ts">
 import { useI18n } from 'vue-i18n';
+import { onMounted, ref } from 'vue';
 
 const { t } = useI18n();
+
+const downloadUrls = ref({
+  windows: '#',
+  mac: '#',
+  linux: '#'
+});
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://api.github.com/repos/th0mate/BRGM-FluoGraphiX_v2/releases/latest');
+    if (!response.ok) {
+      throw new Error(`GitHub API responded with ${response.status}`);
+    }
+    const data = await response.json();
+    data.assets.forEach(asset => {
+      if (asset.name.endsWith('.exe')) {
+        downloadUrls.value.windows = asset.browser_download_url;
+      } else if (asset.name.endsWith('.dmg')) {
+        downloadUrls.value.mac = asset.browser_download_url;
+      } else if (asset.name.endsWith('.AppImage')) {
+        downloadUrls.value.linux = asset.browser_download_url;
+      }
+    });
+  } catch (error) {
+    console.error('Failed to fetch latest release assets:', error);
+  }
+});
 </script>
 
 <template>
@@ -22,17 +50,17 @@ const { t } = useI18n();
         <div class="possibility windows">
           <img src="@/assets/img/icons/windows.png" alt="Windows">
           <h4>{{ t('download.windows') }}</h4>
-          <a href="#" class="bouton boutonFonce">{{ t('buttons.download') }}</a>
+          <a :href="downloadUrls.windows" class="bouton boutonFonce">{{ t('buttons.download') }}</a>
         </div>
         <div class="possibility mac">
           <img src="@/assets/img/icons/mac.png" alt="Mac">
           <h4>{{ t('download.mac') }}</h4>
-          <a href="#" class="bouton boutonFonce">{{ t('buttons.download') }}</a>
+          <a :href="downloadUrls.mac" class="bouton boutonFonce">{{ t('buttons.download') }}</a>
         </div>
         <div class="possibility android">
           <img src="@/assets/img/icons/android.png" alt="Android">
           <h4>{{ t('download.linux') }}</h4>
-          <a href="#" class="bouton boutonFonce">{{ t('buttons.download') }}</a>
+          <a :href="downloadUrls.linux" class="bouton boutonFonce">{{ t('buttons.download') }}</a>
         </div>
       </div>
     </div>
